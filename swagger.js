eb5ba9003537f7444ccc1b6dad08936908b14127
soliverson <stacy.oliverson@gmail.com>
2024-05-18 14:40:09 -0600

    Finished Project 2

--- swagger.js
+++ swagger.js
@@ -10,7 +10,7 @@ const doc = {
 };
 
 const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const endpointsFiles = ['./routes/index.js,' './routes/hymns/js', './routes/gospletopics.js'];
 
 // Generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
